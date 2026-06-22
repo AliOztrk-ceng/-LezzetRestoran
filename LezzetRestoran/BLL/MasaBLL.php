c3afb58ccb20d3fb781d0c9dfd9d0441d82b3789
AliOztrk-ceng <isbirligi78@gmail.com>
2026-06-22 17:42:12 +0300

    Update MasaBLL.php

--- LezzetRestoran/BLL/MasaBLL.php
+++ LezzetRestoran/BLL/MasaBLL.php
@@ -1,14 +1,13 @@
 <?php
 // BLL, DAL ile iletişim kuracağı için MasaDAL dosyamızı çağırıyoruz
-// '../' ifadesi bir üst klasöre çıkıp DAL klasörüne girmemizi sağlar
+
 require_once '../DAL/MasaDAL.php';
 
 class MasaBLL {
     
     // 1. Masaları Listeleme Kontrolü
     public static function masalariGetirBLL() {
-        // Burada ileride "Sadece yetkili kullanıcılar masaları görebilir" gibi 
-        // güvenlik kuralları eklenebilir. Şimdilik doğrudan DAL'a yönlendiriyoruz.
+       
         return MasaDAL::masalariGetir();
     }
 
